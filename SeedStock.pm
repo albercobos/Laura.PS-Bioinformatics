b68b63748d66e800db8302e9c4c0b0a856416c75
LauraPS1 <laura.psantamaria@gmail.com>
2015-02-26 20:48:40 +0100

    Update SeedStock.pm

--- SeedStock.pm
+++ SeedStock.pm
@@ -3,28 +3,32 @@ use strict;
 use Moose;
 
 has 'SeedID' => (		# Seed Identifier
-		is =>'rw' 
-		isa => 'str' 
+	is =>'rw',
+	isa => 'str', 
+	required =>1
 	);
 
 has 'MutantGene' => (		# Mutant Gene Identifier
-		is =>'rw' 
-		isa => 'str' 
+	is =>'rw',
+	isa => 'Gene',
+	required=>1
 	);
 
 has 'LastPlanted' => (		# Last date the seed was planted
-		is =>'rw' 
-		isa => 'date' 
+	is =>'rw', 
+	isa => 'date'
 	);
 
 has 'Storage' => (		# Place where the seed is kept
-		is =>'rw' 
-		isa => 'str' 
+	is =>'rw', 
+	isa => 'str'
 	);	
 
 has 'GramsRemaining' => (		# Amount of seeds in stock
-		is =>'rw' 
-		isa => 'int' 
+	is =>'rw',
+	isa => 'int',
+	required =>1,
+	default=>0 #If any value is given, the amount is set to zero.
 	);
 
 1;
