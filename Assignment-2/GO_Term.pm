0cf3dd143ac30999ee9169b71d3741de657172ed
Laura <laura.psantamaria@gmail.com>
2015-03-13 12:59:39 +0100

    Update GO_Term.pm

--- Assignment-2/GO_Term.pm
+++ Assignment-2/GO_Term.pm
@@ -44,5 +44,31 @@ unless($GO_ID =~ /[GO|Go|go]:\d{7}/){
 	die "Error: The GO ID $GO_ID has the wrong format.";
 }
 
+sub GetGOTerms{ # makes a hash of GO annotation objects from .obo files, with ID numbers and term names.
+my $GO_file = $_[0];
+
+open(IN, "<$GO_file") ||
+            die "can't open the input gene info file!\n";
+
+my $GOid; my $save=0; my %GOHash;
+while (my $line = <IN>) {
+
+	if ($line=~/^id:\s(GO:\d{7})/){
+		$save=1;					# $save variable is 1 until the next line, because 'name' is always in the line after id.
+		$GOid=$1;
+	}
+	if ($save==1 and $line =~ /^name:\s(.*)$/){
+		$save=0;
+		my $name = $1;
+		my $GO_object= GO_Term -> new (
+			ID => "$GOid",
+			Name => "$name"
+			);
+		$GOHash{$GOid}=$GO_object;
+	}
+		
+return %GOHash;
 }
+}
+
 1;
